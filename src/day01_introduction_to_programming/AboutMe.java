b252e627da071167759c0e8b21eff21ec383aafa
CybertekSchool <github@cybertekschool.com>
2019-11-09 17:40:35 -0500

    day1 code

---
+++ src/day01_introduction_to_programming/AboutMe.java
@@ -0,0 +1,10 @@
+package day01_introduction_to_programming;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("Hello, My name is Murodil");
+    }
+}
+
+
+
