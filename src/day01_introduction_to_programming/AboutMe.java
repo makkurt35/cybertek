ef89aed91550b925bebbe6128ab71134c2d96c05
CybertekB15 <cybertekb15@CybertekSchools-iMac-2.local>
2020-01-11 17:10:18 -0500

    day 39 code

--- src/day01_introduction_to_programming/AboutMe.java
+++ src/day01_introduction_to_programming/AboutMe.java
@@ -2,6 +2,7 @@ package day01_introduction_to_programming;
 
 public class AboutMe {
     public static void main(String[] args) {
+
         System.out.println("Hello, My name is Murodil");
     }
 }
---
+++ src/day39_list_practice_custom_classes/Employee.java
@@ -0,0 +1,13 @@
+package day39_list_practice_custom_classes;
+
+public class Employee {
+    //data -> variables
+    String name;
+    int age;
+    String jobTitle;
+
+    //behaviour -> methods
+    public void work(){
+        System.out.println("Employee is working hard.");
+    }
+}
