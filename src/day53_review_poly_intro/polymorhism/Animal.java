6f89ceb294b5b0f8a333633a9b65f94726161f14
CybertekB15 <cybertekb15@CybertekSchools-iMac-2.local>
2020-02-05 21:34:16 -0500

    code

---
+++ src/day53_review_poly_intro/polymorhism/Animal.java
@@ -0,0 +1,9 @@
+package day53_review_poly_intro.polymorhism;
+
+public class Animal {
+    String type;
+    String name;
+    public void speak(){
+        System.out.println("Animal is saying java java...");
+    }
+}
---
+++ src/day53_review_poly_intro/polymorhism/Dog.java
@@ -0,0 +1,12 @@
+package day53_review_poly_intro.polymorhism;
+
+public class Dog extends Animal {
+
+    public void speak(){
+        System.out.println("Dog is saying Woof woof...");
+    }
+
+    public void hunt(){
+        System.out.println("Dog is hunting duck...");
+    }
+}
